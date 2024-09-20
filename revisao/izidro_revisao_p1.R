1d582244f8fa30bce34e3fb1e0bd92482516d4c0
Matheus Izidro C dos Santos <127612958+Izidro015@users.noreply.github.com>
2024-09-20 10:21:02 -0300

    Update izidro_revisao_p1.R

--- revisao/izidro_revisao_p1.R
+++ revisao/izidro_revisao_p1.R
@@ -1,3 +1,7 @@
+#Exercício 1
+#Crie uma função chamada EstimativaIntegral que possua como entradas dois números reais a e b, a < b, e que retorne o valor da integral
+#Por fim, utilize a função anterior para estimar a integral com a = −1 e b = 2.
+
 EstimativaIntegral <- function(a, b) {
   integrand <- function(x) {
     exp(2 * x + x^2)
